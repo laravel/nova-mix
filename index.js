e5c29b77ad6fdc116cd6b13633e0e3141002790c
Mior Muhammad Zaki <crynobone@gmail.com>
2022-01-24 19:41:50 +0800

    first
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+let mix = require('laravel-mix')
+let webpack = require('webpack')
+
+class NovaExtension {
+  name() {
+    return 'nova-extension'
+  }
+
+  register(name) {
+    this.name = name
+  }
+
+  webpackPlugins() {
+    return new webpack.ProvidePlugin({
+      _: 'lodash',
+      axios: 'axios',
+    })
+  }
+
+  webpackConfig(webpackConfig) {
+    webpackConfig.externals = {
+      vue: 'Vue',
+      'laravel-nova': 'Nova.mixins',
+    }
+
+    webpackConfig.output = {
+      uniqueName: this.name,
+    }
+  }
+}
+
+mix.extend('nova', new NovaExtension())
